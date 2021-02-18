3651717ce60c2c0d9f2b1324c36f2e4fd44f4e6b
banksjazz87 <61635862+banksjazz87@users.noreply.github.com>
2021-02-17 21:13:56 -0500

    docs: I modified how the dropdown menu appears.

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@ const menu = document.getElementById("hamburger-container");
 
 const links = document.getElementById('nav-links');
 
+const nav = document.getElementById('navbar');
+
 menu.addEventListener('click', () => {
     if (links.style.display === 'none') {
         links.style.display = 'flex';
