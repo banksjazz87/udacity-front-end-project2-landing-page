cef01a2e86cd91f6244c8a82ab58c24a01b66642
banksjazz87 <61635862+banksjazz87@users.noreply.github.com>
2021-02-18 05:49:56 -0500

    docs: working on a function to create more of an animated dropdown menu.

--- js/app.js
+++ js/app.js
@@ -1,15 +1,25 @@
 //function for the drop down menu
 
+
 const menu = document.getElementById("hamburger-container");
-
 const links = document.getElementById('nav-links');
-
 const nav = document.getElementById('navbar');
+
+let increment = null;
+let stop = clearInterval(increment);
+let i = 0;
 
 menu.addEventListener('click', () => {
     if (links.style.display === 'none') {
         links.style.display = 'flex';
+        increment = setInterval(() => {
+            i++;
+            console.log(i);
+        }, 1000);
+
     } else {
         links.style.display = 'none';
+        stop;
+        i = 0;
     }
 });
